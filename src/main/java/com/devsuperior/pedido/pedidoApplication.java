65b6f18ea363ba68cee48665f42de6d564983e68
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-09 09:53:49 -0300

    Entity Order created

--- src/main/java/com/devsuperior/pedido/pedidoApplication.java
+++ src/main/java/com/devsuperior/pedido/pedidoApplication.java
@@ -1,13 +1,19 @@
 package com.devsuperior.pedido;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class pedidoApplication {
+@SpringBootApplication  
+public class pedidoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(pedidoApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
 	}
 
 }
