9fb4d6c1bf59ce5f40877b1a410dfa86daeb1961
Aline-Pinotti <alinefpinotti@gmail.com>
2025-02-09 09:40:23 -0300

    Project created

---
+++ src/main/java/com/devsuperior/pedido/pedidoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class pedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(pedidoApplication.class, args);
+	}
+
+}
