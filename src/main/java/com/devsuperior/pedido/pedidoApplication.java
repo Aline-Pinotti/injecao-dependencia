b795a7f824c3964181699255d939e1f46bd14fd9
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-09 10:22:20 -0300

    Finish Application

--- src/main/java/com/devsuperior/pedido/pedidoApplication.java
+++ src/main/java/com/devsuperior/pedido/pedidoApplication.java
@@ -1,19 +1,38 @@
 package com.devsuperior.pedido;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  
-public class pedidoApplication implements CommandLineRunner{
+import com.devsuperior.pedido.entities.Order;
+import com.devsuperior.pedido.services.OrderService;
+
+@SpringBootApplication
+public class pedidoApplication implements CommandLineRunner {
+
+	@Autowired
+	public OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(pedidoApplication.class, args);
 	}
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+
+		Order order1 = new Order(1034, 150.00, 20.0);
+		Order order2 = new Order(2282, 800.00, 10.0);
+		Order order3 = new Order(1309, 95.90, 0.0);
+
+		System.out.printf("Pedido código %d", order1.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order1));
+
+		System.out.printf("Pedido código %d", order2.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order2));
+
+		System.out.printf("Pedido código %d", order3.getCode());
+		System.out.printf("Valor total: R$ %.2f%n", orderService.total(order3));
 	}
 
 }
