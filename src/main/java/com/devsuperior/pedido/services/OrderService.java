a97469eb6a671f3b0c043e0782a0454876802236
user.name: Aline-Pinotti <user.email: alinefpinotti@gmail.com>
2025-02-09 10:07:57 -0300

    OrderService created

--- src/main/java/com/devsuperior/pedido/services/OrderService.java
+++ src/main/java/com/devsuperior/pedido/services/OrderService.java
@@ -1,8 +1,20 @@
 package com.devsuperior.pedido.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.devsuperior.pedido.entities.Order;
 
 @Service
 public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public double total(Order order) {
+        double shippingCost = shippingService.shipment(order);
+        double discount = order.getBasic() * (order.getDiscount() / 100);
+        return order.getBasic() - discount + shippingCost;
+    }
 
 }
